bb8af436eb4309d3d434c8febd8a1619f104d138
charles silva <charlesilva2323@gmail.com>
2024-10-30 18:56:38 -0300

    feat: home page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,18 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        brand: {
+          blue: "hsla(221, 100%, 63%, 1)",
+          darkBlue: "hsla(221, 100%, 30%, 1)",
+          gray: "hsla(0, 0%, 62%, 1)"
+        }
       },
+
     },
+   
+    boxShadow: {
+      'card': '2px 2px 30px hsla(0, 0%, 0%, 0.25)',
+      },
   },
   plugins: [],
 };
