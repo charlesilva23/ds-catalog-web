c9d22edb23dc792f6ae6b3b4ff1eeb549bf7a8a8
charles silva <charlesilva2323@gmail.com>
2024-10-30 19:09:49 -0300

    fix: fixing build errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex justify-center items-center h-screen py-10">
@@ -6,14 +8,14 @@ export default function Home() {
           <label>Conheça o melhor catálogo de produtos</label>
         </div>
         <div className="absolute left-[40%] ml-10 mt-20">
-          <img src="/assets/Desenho.png" alt="desenho" width={648} height={480} />
+          <Image src="/assets/Desenho.png" alt="desenho" width={648} height={480} />
         </div>
         <div className="relative w-1/3 top-1/4 left-16 py-10 text-brand-gray">
           <span>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</span>
         </div>
         <div className="mt-auto flex relative pb-24 left-16"> 
           <button className="bg-brand-darkBlue h-14 w-[30%] border rounded-lg">
-          <img src="/assets/arrow.svg" alt="arrow" className="absolute left-1/4 mt-4 ml-5"/>
+          <Image src="/assets/arrow.svg" alt="arrow" width={10} height={20} className="absolute left-1/4 mt-4 ml-5"/>
             <div className="flex items-center justify-center bg-brand-blue text-white w-4/5 h-full rounded-tl-lg rounded-bl-lg">
               <label htmlFor="buscar">INICIE AGORA A SUA BUSCA</label>
             </div>
