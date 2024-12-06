cc8301b361f366738680c02864a4848a50222b8c
charles silva <charlesilva2323@gmail.com>
2024-12-06 19:01:27 -0300

    fix: fixing product detail

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -3,37 +3,33 @@
 import Card from "@/components/Cards";
 import { getProductById } from "@/services/products.service";
 import { IProductsRecordDTO } from "@/utils/products";
+import { useParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 
-interface ProductDetailsProps {
-  product: IProductsRecordDTO
-}
-
-const ProductDetails = ({ product }: ProductDetailsProps) => {
+const ProductDetails = () => {
   const [productById, setProductById] = useState<IProductsRecordDTO | null>(null)
-
+  const { id } = useParams()
   useEffect(() => {
-    console.log("produto", product)
+
     const fetchProductsById = async () => {
       try {
-        const response = await getProductById(product.id)
-        console.log(response)
+        const response = await getProductById(Number(id))
         setProductById(response)
       } catch (error) {
         console.error(error)
       }
     }
     fetchProductsById()
-  }, [product])
+  }, [id])
 
 
   return (
     <div>
       <Card variant="mdCard" className="flex w-full h-full">
-        <h1>{product?.id}</h1>
-        <h1>{product?.name}</h1>
-        <h1>{product?.price}</h1>
+        <h1>{productById?.id}</h1>
+        <h1>{productById?.name}</h1>
+        <h1>{productById?.price}</h1>
       </Card>
     </div>
   )
