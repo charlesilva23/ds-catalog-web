fdf731d402d4e6ca153ea0464afc79a4ec50233e
Charles Silva <charlesilva2323@gmail.com>
2025-05-26 13:32:29 -0300

    fix: downgrade react to stable version for Vercel build

--- src/store/auth.ts
+++ src/store/auth.ts
@@ -1,27 +1,24 @@
+// import { getCookie } from "@/utils/cookies";
 
+// export interface AuthState {
+//   isAuthenticated: boolean;
+//   authenticate: (data: { accessToken: string; refreshToken: string }) => void;
+//   checkAuthentication: () => void;
+//   logout: () => void;
+//   refreshToken: () => Promise<void>;
+// }
 
+// export const useAuth = create<AuthState>((set) => ({
+//   isAuthenticated: false,
+//   authenticate: (data) => {
+//     setCookie("@dsCatalog:accessToken", data.accessToken, 30);
+//     setCookie("@dsCatalog:refreshToken", data.refreshToken, 7);
+//     api.defaults.Authorization = `Bearer ${data.accessToken}`;
+//     set({ isAuthenticated: true });
+//   },
 
-export interface AuthState {
-    isAuthenticated: boolean,
-    authenticate: (data: { accessToken: string, refreshToken: string }) => void,
-    checkAuthentication: () => void,
-    logout: () => void,
-    refreshToken: () => Promise<void>
-}
-
-export const useAuth = create<AuthState>((set) => ({
-    isAuthenticated: false,
-    authenticate: (data) => {
-        setCookie('@dsCatalog:accessToken', data.accessToken, 30)
-        setCookie('@dsCatalog:refreshToken', data.refreshToken, 7)
-        api.defaults.Authorization = `Bearer ${data.accessToken}`
-        set({ isAuthenticated: true })
-    },
-
-    checkAuthentication: () => {
-        const accessToken = getCookie('@dsCatalog:accessToken')
-        api.defaults.Authorization = `Bearer ${accessToken}`
-    }
-
-
-}))
+//   checkAuthentication: () => {
+//     const accessToken = getCookie("@dsCatalog:accessToken");
+//     api.defaults.Authorization = `Bearer ${accessToken}`;
+//   },
+// }));
